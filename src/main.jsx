7bb06ad001dc7d60565eb7c5d6bed7e35f960b1b
Marcelo Lourenco <marcelo.lourenco@sportbay.com.br>
2026-02-11 16:17:51 -0300

    teste

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme";
-import { GlobalStyle } from "./styles/Global.js";
+import { GlobalStyle } from "./styles/GlobalStyle.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
