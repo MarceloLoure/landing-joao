e04debb08dcb8b851fb964b3ec2ce12bee8e2dc0
Marcelo Lourenco <marcelo.lourenco@sportbay.com.br>
2026-02-11 16:13:16 -0300

    primeiro commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme";
-import { GlobalStyle } from "./styles/Global";
+import { GlobalStyle } from "./styles/Global.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
